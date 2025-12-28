a1cede7175b00c07c38b2642d4828085ccbff9f5
Divyansh Khatri <divyanshkhatri200102@gmail.com>
2025-12-28 07:09:33 +0000

    fix: indentation fix
    Signed-off-by: Divyansh Khatri <divyanshkhatri200102@gmail.com>

--- cmake/WASINNDeps.cmake
+++ cmake/WASINNDeps.cmake
@@ -417,24 +417,25 @@ function(wasmedge_setup_piper_target target)
       endif()
     endif()
   endif()
-  if(NOT onnxruntime_FOUND)
+if(NOT onnxruntime_FOUND)
     message(FATAL_ERROR "Cannot find onnxruntime")
   endif()
+
   if(NOT TARGET piper)
     # setup piper
-    if (DEFINED PIPER_ROOT)
+    if(DEFINED PIPER_ROOT)
       message(STATUS "Build: Using pre-built Piper from ${PIPER_ROOT}")
       add_library(piper STATIC IMPORTED)
       find_library(PIPER_LIB_PATH
-          NAMES piper libpiper
-          PATHS "${PIPER_ROOT}/"
-            NO_DEFAULT_PATH
-            REQUIRED
-        )
-        set_target_properties(piper PROPERTIES
-            IMPORTED_LOCATION "${PIPER_LIB_PATH}"
-            INTERFACE_INCLUDE_DIRECTORIES "${PIPER_ROOT}/include"
-        )
+        NAMES piper libpiper
+        PATHS "${PIPER_ROOT}/"
+        NO_DEFAULT_PATH
+        REQUIRED
+      )
+      set_target_properties(piper PROPERTIES
+        IMPORTED_LOCATION "${PIPER_LIB_PATH}"
+        INTERFACE_INCLUDE_DIRECTORIES "${PIPER_ROOT}/include"
+      )
     else()
       message(STATUS "Downloading piper source")
       find_program(GIT_CMD git REQUIRED)
@@ -448,12 +449,15 @@ function(wasmedge_setup_piper_target target)
       set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Piper not build shared")
       set(BUILD_TESTING OFF CACHE INTERNAL "Piper not build tests")
       set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE INTERNAL "Piper build independent code")
+      
       FetchContent_MakeAvailable(piper)
       message(STATUS "Downloading piper source -- done")
+      
       # suppress src/cpp/piper.cpp:302:29: error: unused parameter ‘config’ [-Werror=unused-parameter]
       target_compile_options(piper PRIVATE -Wno-error=unused-parameter)
-      endif()
+    endif()
   endif()
+
   wasmedge_setup_simdjson()
   target_link_libraries(${target}
     PRIVATE
