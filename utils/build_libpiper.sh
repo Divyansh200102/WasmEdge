1ceb90caab9406cadc503eed0913263f780c8762
Divyansh Khatri <divyanshkhatri200102@gmail.com>
2025-12-28 08:26:35 +0000

    fix: add build script and fix indentation
    Signed-off-by: Divyansh Khatri <divyanshkhatri200102@gmail.com>

---
+++ utils/build_libpiper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "::group::Build and install libpiper"
+rm -rf piper-source
+
+git clone https://github.com/OHF-Voice/piper1-gpl piper-source
+cd piper-source/libpiper
+
+cmake -Bbuild-deps \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="$PWD/install" \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+
+cmake --build build-deps
+cmake --install build-deps
+echo "::endgroup::"
