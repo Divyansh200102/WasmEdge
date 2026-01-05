a17b4226c28a34328c6eca911e37070d5a548629
Divyansh Khatri <divyanshkhatri200102@gmail.com>
2026-01-05 08:34:00 +0000

    fix: pin libpiper commit and add arch validation
    Signed-off-by: Divyansh Khatri <divyanshkhatri200102@gmail.com>

--- utils/wasi-nn/install-libpiper.sh
+++ utils/wasi-nn/install-libpiper.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: 2019-2024 Second State INC
+# SPDX-FileCopyrightText: 2019-2026 Second State INC
 
 set -e
 
-git clone https://github.com/OHF-Voice/piper1-gpl.git piper-source
-cd piper-source/libpiper
+PIPER_REPO="https://github.com/OHF-Voice/piper1-gpl.git"
+PIPER_COMMIT="32b95f8c1f0dc0ce27a6acd1143de331f61af777"
+PIPER_INSTALL_TO="/usr/local"
 
-cmake -Bbuild \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DONNXRUNTIME_DIR=/usr/local \
+case "$(uname -m)" in
+  'x86_64') ;;
+  'aarch64') ;;
+  *)
+    echo "Unsupported architecture for libpiper: $(uname -m)" >&2
+    exit 1
+    ;;
+esac
 
+git clone --depth 1 "${PIPER_REPO}" piper-source
+cd piper-source
+git fetch --depth 1 origin "${PIPER_COMMIT}"
+git checkout FETCH_HEAD
+
+cd libpiper
+
+cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PIPER_INSTALL_TO}"
 cmake --build build
 cmake --install build
 
 cd ../..
 rm -rf piper-source
+
+ldconfig
